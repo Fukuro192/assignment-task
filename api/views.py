991d46c6e0514015061651abba8909f5895c18b2
Fukuro192 <salah8elmes.2000@gmail.com>
2022-11-19 14:34:28 +0100

    fix lower case search problem

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ def get_weather(request):
     data = pd.read_csv('cities.csv', sep=";")
     if request.method == 'GET' and 'city' in request.GET:
         city = request.GET['city']
-        data = data[data['city'].str.lower().str.contains(city)]
+        data = data[data['city'].str.lower().str.contains(city.lower())]
     json_format = data.to_json(orient="records")
     weather = json.loads(json_format)
     return JsonResponse({'weather': weather})
